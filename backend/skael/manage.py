13e0187493c5830efd3be652636bd07900b91a5f
devm <servicemanager3386@yandex.com>
2017-08-01 07:38:31 -0400

    add dropdb command

--- backend/skael/manage.py
+++ backend/skael/manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 from skael.skael import create_app
 
@@ -7,6 +7,12 @@ app = create_app()
 manager = Manager(app)
 migrate = Migrate(app, app.db)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def dropdb():
+    if prompt_bool(
+        "Are you sure you want to lose all your data"):
+        app.db.drop_all()
 
 if __name__ == '__main__':
     manager.run()
